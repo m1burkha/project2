8a41265bcfc5a8866bb8e206aa72c46cce1a10aa
m1burkha <maritn.burkhard@hsr.ch>
2018-02-10 14:32:24 +0100

    fixes ts import

--- dienstplaner/src/app/services/shift-scheduling/shift-scheduling.service.ts
+++ dienstplaner/src/app/services/shift-scheduling/shift-scheduling.service.ts
@@ -2,7 +2,8 @@ import {Injectable} from '@angular/core';
 import {FirestoreService} from '@services/firestore/firestore.service';
 import {AngularFirestore} from 'angularfire2/firestore';
 import {IShiftSchedule} from '@domain-models/shift-scheduling/shift-schedule';
-import moment = require('moment');
+import * as moment from "moment";
+
 
 /**
  * shift scheduling service
