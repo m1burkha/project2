12b053b8570188af5e64b94acd554bff7116953c
Marc-Alexander Iten <marc-alexander.iten@htwchur.ch>
2018-04-17 14:35:45 +0200

    presentation

--- dienstplaner/src/app/app-routing.module.ts
+++ dienstplaner/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from '@components/login/login.component';
 import {TestComponent} from '@components/db/test/test.component';
 import {LogoutComponent} from '@components/logout/logout.component';
+import {PresentationComponent} from '@components/presentation/presentation.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: 'logout', component: LogoutComponent},
   {path: 'shiftlist', loadChildren: 'app/components/shift-planner/shift-planner.module#ShiftPlannerModule'},
   {path: 'employees', loadChildren: 'app/components/employee-administration/employee-administration.module#EmployeeAdministrationModule'},
+  {path: 'presentation', component: PresentationComponent},
   {path: 'dev/click-dummy', loadChildren: 'app/components/dev/click-dummy/click-dummy.module#ClickDummyModule'},
   {path: 'db/test', component: TestComponent}
 ];
